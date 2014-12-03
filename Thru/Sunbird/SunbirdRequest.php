baf77de557aceef14734484e756fd7dd34426026
Matthew Baggett <matthew@baggett.me>
2014-12-03 23:25:18 +0000

    Change namespace.

--- Thru/Sunbird/SunbirdRequest.php
+++ Thru/Sunbird/SunbirdRequest.php
@@ -16,7 +16,7 @@ class SunbirdRequest{
   }
 
   private function get_type(){
-    return strtolower(str_replace("Fapla\\Models\\Sunbird","", get_called_class()));
+    return strtolower(str_replace("Thru\\Sunbird\\Sunbird","", get_called_class()));
   }
   private function get_endpoint(){
     return SUNBIRD_HOST . "/" . SUNBIRD_API_VERSION . "/" . SUNBIRD_API_KEY . "/" . $this->get_type();
@@ -26,7 +26,7 @@ class SunbirdRequest{
     $response = $this->request('get');
     $list = ucfirst($this->get_type());
     $output = [];
-    $child_class = "Fapla\\Models\\Sunbird" . Inflect::singularize($list);
+    $child_class = "Thru\\Sunbird\\" . Inflect::singularize($list);
     $child_class_exists = class_exists($child_class);
 
     foreach($response->$list as $k => $element){
@@ -45,7 +45,7 @@ class SunbirdRequest{
     $endpoint = $this->get_endpoint();
     $response = $this->curl->get($endpoint);
     if($this->curl->get_status() != 200){
-      throw new SunbirdException("Cannot talk to Sunbird, response was {$this->curl->get_status()}");
+      throw new SunbirdException("Cannot talk to Sunbird, response was {$this->curl->get_status()} while requesting {$endpoint}");
     }
     $response = json_decode($response);
     if($response == false){
